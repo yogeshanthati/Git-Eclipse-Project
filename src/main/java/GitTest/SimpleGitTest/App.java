c8e71c5a6049cc31dfab3b334438b595d9c54fa5
yogeshanthati <yogesh946@gmail.com>
2020-04-08 15:58:31 +0530

    my first commit as leader

---
+++ src/main/java/GitTest/SimpleGitTest/App.java
@@ -0,0 +1,20 @@
+package GitTest.SimpleGitTest;
+
+/**
+ * Hello world!lead code
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+
+    {  
+	 System.out.println("Lead has created this line remaining yogesh and sriveni create ");
+
+  
+
+	
+
+
+    }
+}
